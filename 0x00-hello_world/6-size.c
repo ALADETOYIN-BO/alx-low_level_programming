5222bc30fcb41ac60768f646b2c8d0c94322f089
ALADETOYIN-BO <okeowobee11@gmail.com>
2022-09-08 18:44:22 -0700

    good

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
 
 /**
+ *main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: always 0
- *
- *     */
-
+ *Return: always 0
+ */
 int main(void)
-
 {
 
 		printf("Size of a char: %d byte(s)\n", sizeof(char));

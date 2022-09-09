5c01843b02a5220354f21ba028755b35082e1294
ALADETOYIN-BO <okeowobee11@gmail.com>
2022-09-08 19:26:12 -0700

    good

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,15 @@
-#include <stdio.h>
+#include<stdio.h>
+
 /**
- * main - A function that prints stuff
- * Return: Prove that it is successful
- */
+ * main - Entry point
+ *
+ * Description: A C program that prints with printf function.
+ *
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }       
